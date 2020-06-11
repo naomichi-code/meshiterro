a5d9bcd5ad979a90f554f16040ad1765e1156508
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-06-11 20:41:47 +0900

    add index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c760f2b88a892e610488464863cbbc9dd3cad9a27bbf06c789d0241063964c9c55cc8b3dc962165c1d256a43dbe1f198ae758641497dfe155ccb0986d8c3f55b'
